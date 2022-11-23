51c1bb50f02a9f093e2ae78a265e4c27e1120f43
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2022-11-22 22:11:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -124,3 +124,23 @@ function change() {
   }
 }
 change();
+
+// REVEALING SCHEDULES ANIMATION
+const allSections = document.querySelector('.main-schedules');
+
+const revealSection = function (entries) {
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+})
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+});
